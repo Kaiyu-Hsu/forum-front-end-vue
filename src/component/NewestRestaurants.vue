af1bbc2d3169598cb0f8f8f17a6c7a1d034f5686
Kaiyu <anne550323@gmail.com>
2021-08-27 22:30:57 +0800

    feat: install moment & add filters function

--- src/component/NewestRestaurants.vue
+++ src/component/NewestRestaurants.vue
@@ -13,7 +13,7 @@
           <small>{{ restaurant.Category ? restaurant.Category.name : "未分類" }}</small>
         </h4>
         <p>{{ restaurant.description }}</p>
-        {{ restaurant.createdAt }}
+        {{ restaurant.createdAt | fromNow }}
         <hr>
       </div>
     </div>
@@ -21,7 +21,17 @@
 </template>
 
 <script>
+import moment from 'moment'
 export default {
+    filters: {
+        fromNow(datetime) {
+            if(!datetime) {
+                return '-'
+            }
+            // 使用 moment 提供的 fromNow 方法
+            return  moment(datetime).fromNow()
+        }
+    },
     props: {
         restaurants: {
             type: Array,
