8d773133339d3130fc4253bfef3bddc55b02f7c8
Kaiyu <anne550323@gmail.com>
2021-08-28 21:22:40 +0800

    feat: add RestaurantDetail component in Restaurant route

---
+++ src/component/RestaurantDetail.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="row">
+    <div class="col-md-12 mb-3">
+      <h1>Judy Runte</h1>
+      <p class="badge badge-secondary mt-1 mb-3">義大利料理</p>
+    </div>
+    <div class="col-lg-4">
+      <img
+        class="img-responsive center-block"
+        src="https://loremflickr.com/320/240/food,dessert,restaurant/"
+        style="width: 250px; margin-bottom: 25px"
+      />
+      <div class="contact-info-wrap">
+        <ul class="list-unstyled">
+          <li>
+            <strong>Opening Hour:</strong>
+            08:00
+          </li>
+          <li>
+            <strong>Tel:</strong>
+            (918) 827-1962
+          </li>
+          <li>
+            <strong>Address:</strong>
+            98138 Elisa Road
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-lg-8">
+      <p>{{ restaurant.description }}</p>
+      <a class="btn btn-primary btn-border mr-2" href="#">Dashboard</a>
+
+      <button
+        v-if="restaurant.isFavorited"
+        @click="deleteFavorite"
+        type="button"
+        class="btn btn-danger btn-border mr-2"
+      >
+        移除最愛
+      </button>
+      <button
+        v-else
+        @click="addFavorite"
+        type="button"
+        class="btn btn-primary btn-border mr-2"
+      >
+        加到最愛
+      </button>
+      <button
+        v-if="restaurant.isLiked"
+        @click="deleteLiked"
+        type="button"
+        class="btn btn-danger like mr-2"
+      >
+        Unlike
+      </button>
+      <button
+        v-else
+        @click="addLiked"
+        type="button"
+        class="btn btn-primary like mr-2"
+      >
+        Like
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    restaurant: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    addFavorite() {
+      this.restaurant = {
+        ...this.restaurant, // 保留餐廳內原有資料
+        isFavorited: true,
+      };
+    },
+    deleteFavorite() {
+      this.restaurant = {
+        ...this.restaurant,
+        isFavorited: false,
+      };
+    },
+    addLiked() {
+      this.restaurant = {
+        ...this.restaurant,
+        isLiked: true,
+      };
+    },
+    deleteLiked() {
+      this.restaurant = {
+        ...this.restaurant,
+        isLiked: false,
+      };
+    },
+  },
+};
+</script>
