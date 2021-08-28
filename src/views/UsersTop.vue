c80b2a2ebc17d8b3f2895bebb5fa0311f6e121ba
Kaiyu <anne550323@gmail.com>
2021-08-28 23:20:19 +0800

    feat: refactor UsersTop & add UserCards component

--- src/views/UsersTop.vue
+++ src/views/UsersTop.vue
@@ -4,41 +4,14 @@
     <h1 class="mt-5">美食達人</h1>
     <hr />
     <div class="row text-center">
-      <div class="col-3" 
-      v-for="user in users" 
-      :key="user.id">
-        <a href="#">
-          <img :src="user.image" width="140px" height="140px" />
-        </a>
-        <h2>{{ user.name }}</h2>
-        <span class="badge badge-secondary"
-          >追蹤人數：{{ user.FollowerCount }}</span
-        >
-        <p class="mt-3">
-          <button
-            v-if="user.isFollowed"
-            @click.stop.prevent="deleteFollowed"
-            type="button"
-            class="btn btn-danger"
-          >
-            取消追蹤
-          </button>
-          <button
-            v-else
-            @click.stop.prevent="addFollowed"
-            type="button"
-            class="btn btn-primary"
-          >
-            追蹤
-          </button>
-        </p>
-      </div>
+      <UserCards v-for="user in users" :key="user.id" :initial-user="user" />
     </div>
   </div>
 </template>
 
 <script>
 import NavTabs from "../component/NavTabs.vue";
+import UserCards from "../component/UserCards.vue";
 
 const dummyUsers = {
   users: [
@@ -455,6 +428,7 @@ export default {
   name: "UsersTop",
   components: {
     NavTabs,
+    UserCards,
   },
   data() {
     return {
@@ -464,22 +438,6 @@ export default {
   methods: {
     fetchUsers() {
       this.users = dummyUsers.users;
-    },
-    addFollowed() {
-      // console.log("addFollowed")
-      this.user = {
-        ...this.user, // 保留餐廳內原有資料
-        isFollowed: true,
-      };
-      console.log("addFollowed");
-    },
-    deleteFollowed() {
-      // console.log("deleteFollowed")
-      this.user = {
-        ...this.user, // 保留餐廳內原有資料
-        isFollowed: false,
-      };
-      console.log("deleteFollowed");
     },
   },
   created() {
