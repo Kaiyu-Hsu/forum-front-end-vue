ba1dcf3426bbdc31f05caaa06c02291f852e1827
Kaiyu <anne550323@gmail.com>
2021-08-30 18:30:24 +0800

    feat: add User route and each components

---
+++ src/component/UserProfileCard.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="card mb-3">
+    <div class="row no-gutters">
+      <div class="col-md-4">
+        <img :src="profile.image" width="300px" height="300px" />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">{{ profile.name }}</h5>
+          <p class="card-text">{{ profile.email }}</p>
+          <ul class="list-unstyled list-inline">
+            <li>
+              <strong>{{ profile.Comments.length }}</strong> 已評論餐廳
+            </li>
+            <li>
+              <strong>{{ profile.FavoritedRestaurants.length }}</strong>
+              收藏的餐廳
+            </li>
+            <li>
+              <strong>{{ profile.Followings.length }}</strong> followings
+              (追蹤者)
+            </li>
+            <li>
+              <strong>{{ profile.Followers.length }}</strong> followers (追隨者)
+            </li>
+          </ul>
+          <p>
+            <template v-if="currentUser.isAdmin">
+              <button class="btn btn-primary">edit</button>
+            </template>
+            <template v-else>
+              <button
+                v-if="profile.isFollowed"
+                @click.stop.prevent="deleteFollowing"
+                type="submit"
+                class="btn btn-danger"
+              >
+                取消追蹤
+              </button>
+              <button
+                v-else
+                @click.stop.prevent="addFollowing"
+                type="submit"
+                class="btn btn-primary"
+              >
+                追蹤
+              </button>
+            </template>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const dummyUser = {
+  currentUser: {
+    id: 1,
+    name: "root123",
+    email: "root@example.com",
+    image: "https://i.imgur.com/WMsHuNP.jpeg",
+    isAdmin: true,
+  },
+  isAuthenticated: true,
+}
+
+export default {
+  props: {
+    initialProfile: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      prfile: this.initialProfile,
+      currentUser: dummyUser.currentUser
+    };
+  },
+  methods: {
+    addFollowing() {
+      this.prfile = {
+        ...this.prfile,
+        isFollowed: true,
+      };
+    },
+    deleteFollowing() {
+      this.prfile = {
+        ...this.prfile,
+        isFollowed: false,
+      };
+    },
+  },
+};
+</script>
