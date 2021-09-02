5f1f31a06b991b89d16edefb614945a23277f53c
Kaiyu <anne550323@gmail.com>
2021-09-02 16:13:36 +0800

    feat: add admitUsers route & function

---
+++ src/component/AdminUsersTable.vue
@@ -0,0 +1,58 @@
+<template>
+  <tbody>
+    <tr>
+      <th scope="row">{{ user.id }}</th>
+      <td>{{ user.email }}</td>
+      <td v-if="user.isAdmin">admin</td>
+      <td v-else>user</td>
+      <td>
+        <button
+          v-if="user.isAdmin"
+          @click.stop.prevent="changeToUser"
+          type="button"
+          class="btn btn-link"
+        >
+          set as user
+        </button>
+        <button
+          v-else
+          type="button"
+          @click.stop.prevent="changeToAdmin"
+          class="btn btn-link"
+        >
+          set as admin
+        </button>
+      </td>
+    </tr>
+  </tbody>
+</template>
+
+<script>
+export default {
+  props: {
+    initialUser: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      user: this.initialUser,
+    };
+  },
+  methods: {
+    changeToUser() {
+      this.user = {
+        ...this.user,
+        isAdmin: false,
+      };
+    },
+    changeToAdmin() {
+      this.user = {
+        ...this.user,
+        isAdmin: true,
+      };
+    },
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -96,6 +96,11 @@ const router = new VueRouter({
       component: () => import('../views/AdminCategories.vue')
     },
     {
+      path: '/admin/users',
+      name: 'admin-users',
+      component: () => import('../views/AdminUsers.vue')
+    },
+    {
       path: '*',
       name: 'not-found',
       component: NotFound,
