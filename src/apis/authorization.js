2e1a3bf38bcbcda65766276e289d2ce3694eef39
Kaiyu <anne550323@gmail.com>
2021-09-09 22:24:57 +0800

    fix: apiHelper拼錯

--- src/apis/authorization.js
+++ src/apis/authorization.js
@@ -1,10 +1,10 @@
-import { apiHepler } from "../utils/helpers";
+import { apiHelper } from "../utils/helpers";
 
 export default {
     //帶入需要的參數
     signIn({ email, password }) {
         // 這裡 return 的會是一個 Promise
-        return apiHepler.post('/signin', {
+        return apiHelper.post('/signin', {
             email,
             password
         })
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,7 +3,7 @@ import Swal from 'sweetalert2'
 
 const baseURL = 'https://forum-express-api.herokuapp.com/api'
 
-export const apiHepler = axios.create({
+export const apiHelper = axios.create({
     baseURL
 })
 
