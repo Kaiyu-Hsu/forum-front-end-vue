a9c5c231cdcf01cab256879014daea5a0f6acfac
Kaiyu <anne550323@gmail.com>
2021-10-20 17:12:15 +0800

    add: basic information of current user

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,33 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
+  // data
   state: {
+    currentUser: {
+      id: -1,
+      name: "",
+      email: "",
+      image: "",
+      isAdmin: false,
+    },
+    isAuthenticated: false, // 是否驗證過
   },
+  // 修改 state 的方法，類似 methods，但只限修改 state
   mutations: {
+    setCurrentUser(state, currentUser) {
+      state.currentUser = {
+        ...state.currentUser,
+        // 從 api 取得的 currentUser
+        ...currentUser,
+      };
+
+      state.isAuthenticated = true;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  // 透過 api 請求資料
+  actions: {},
+  modules: {},
+});
