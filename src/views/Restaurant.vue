ec4fc0c42f89ec481a947f71b740b0f3c07b5c65
Kaiyu <anne550323@gmail.com>
2021-10-21 16:16:27 +0800

    add: get the data of restaurant from api add: get currentUser from vuex

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -2,90 +2,35 @@
   <div class="container py-5">
     <h1>餐廳描述頁</h1>
     <!-- 餐廳資訊頁 RestaurantDetail -->
-    <RestaurantDetail 
-     :initial-restaurant="restaurant"
-     />
+    <RestaurantDetail :initial-restaurant="restaurant" />
     <hr />
     <!-- 餐廳評論 RestaurantComments -->
-    <RestaurantComments 
+    <RestaurantComments
       :restaurant-comments="restaurantComments"
-      @after-delete-comment="afterDeleteComment"/>
+      @after-delete-comment="afterDeleteComment"
+    />
     <!-- 新增評論 CreateComment -->
-    <CreateComment 
-    :restaurant-id="restaurant.id"
-    @after-create-comment="afterCreateComment"
+    <CreateComment
+      :restaurant-id="restaurant.id"
+      @after-create-comment="afterCreateComment"
     />
   </div>
 </template>
 
 <script>
-import RestaurantDetail from '../component/RestaurantDetail.vue'
-import RestaurantComments from '../component/RestaurantComments.vue'
-import CreateComment from '../component/CreateComment.vue'
-
-const dummyData = {
-  restaurant: {
-    id: 1,
-    name: "Judy Runte",
-    tel: "(918) 827-1962",
-    address: "98138 Elisa Road",
-    opening_hours: "08:00",
-    description: "dicta et cupiditate",
-    image: "https://loremflickr.com/320/240/food,dessert,restaurant/?random=1",
-    createdAt: "2019-06-22T09:00:43.000Z",
-    updatedAt: "2019-06-22T09:00:43.000Z",
-    CategoryId: 3,
-    Category: {
-      id: 3,
-      name: "義大利料理",
-      createdAt: "2019-06-22T09:00:43.000Z",
-      updatedAt: "2019-06-22T09:00:43.000Z",
-    },
-    FavoritedUsers: [],
-    LikedUsers: [],
-    Comments: [
-      {
-        id: 3,
-        text: "Quos asperiores in nostrum cupiditate excepturi aspernatur.",
-        UserId: 2,
-        RestaurantId: 1,
-        createdAt: "2019-06-22T09:00:43.000Z",
-        updatedAt: "2019-06-22T09:00:43.000Z",
-        User: {
-          id: 2,
-          name: "user1",
-          email: "user1@example.com",
-          password:
-            "$2a$10$0ISHJI48xu/VRNVmEeycFe8v5ChyT305f8KaJVIhumu7M/eKAikkm",
-          image: "https://i.imgur.com/XooCt5K.png",
-          isAdmin: false,
-          createdAt: "2019-06-22T09:00:43.000Z",
-          updatedAt: "2019-06-23T01:16:31.000Z",
-        },
-      },
-    ],
-  },
-  isFavorited: false,
-  isLiked: false,
-};
-
-const dummyUser = {
-  currentUser: {
-    "id": 1,
-    "name": "root123",
-    "email": "root@example.com",
-    "image": "https://i.imgur.com/WMsHuNP.jpeg",
-    "isAdmin": true
-},
-  isAuthenticated: true
-}
+import RestaurantDetail from "../component/RestaurantDetail.vue";
+import RestaurantComments from "../component/RestaurantComments.vue";
+import CreateComment from "../component/CreateComment.vue";
+import restaurantAPI from "./../apis/restaurants";
+import { Toast } from "./../utils/helpers";
+import { mapState } from "vuex";
 
 export default {
-  name: 'Restaurant',
+  name: "Restaurant",
   components: {
-      RestaurantDetail,
-      RestaurantComments,
-      CreateComment,
+    RestaurantDetail,
+    RestaurantComments,
+    CreateComment,
   },
   data() {
     return {
@@ -102,51 +47,81 @@ export default {
         isLiked: false,
       },
       restaurantComments: [],
-      currentUser: dummyUser.currentUser,
     };
   },
   methods: {
-    fetchRestaurant(restaurantId) {
-      console.log("fetchRestaurant id: ", restaurantId);
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data } = await restaurantAPI.getRestaurant({ restaurantId });
+        console.log("data", data);
 
-      this.restaurant = {
-        id: dummyData.restaurant.id,
-        name: dummyData.restaurant.name,
-        categoryName: dummyData.restaurant.Category.name,
-        image: dummyData.restaurant.image,
-        openingHours: dummyData.restaurant.opening_hours,
-        tel: dummyData.restaurant.tel,
-        address: dummyData.restaurant.address,
-        description: dummyData.restaurant.description,
-        isFavorited: dummyData.isFavorited,
-        isLiked: dummyData.isLiked,
-      };
+        const { restaurant, isFavorited, isLiked } = data;
 
-      this.restaurantComments = dummyData.restaurant.Comments;
+        const {
+          id,
+          name,
+          Category,
+          image,
+          opening_hours: openingHours,
+          tel,
+          address,
+          description,
+          Comments,
+        } = restaurant;
+
+        this.restaurant = {
+          id,
+          name,
+          categoryName: Category ? Category.name : "未分類",
+          image,
+          openingHours,
+          tel,
+          address,
+          description,
+          isFavorited,
+          isLiked,
+        };
+
+        this.restaurantComments = Comments;
+      } catch (error) {
+        console.error(error);
+        Toast.fire({
+          icon: "error",
+          title: "無法取得餐廳資料",
+        });
+      }
     },
     afterDeleteComment(commentId) {
       // 以 filter 保留未被選擇的 comment.id
       this.restaurantComments = this.restaurantComments.filter(
-        comment => comment.id !== commentId
-        )
+        (comment) => comment.id !== commentId
+      );
     },
     afterCreateComment(payload) {
-      const { commentId, restaurantId, text} = payload
+      const { commentId, restaurantId, text } = payload;
       this.restaurantComments.push({
         id: commentId,
         RestaurantId: restaurantId,
         User: {
           id: this.currentUser.id,
-          name: this.currentUser.name
+          name: this.currentUser.name,
         },
         text,
-        createdAt: new Date()
-      })
-    }
+        createdAt: new Date(),
+      });
+    },
+  },
+  computed: {
+    ...mapState(["currentUser", "isAuthenticated"]),
+  },
+  beforeRouteUpdate(to, from, next) {
+    const { id: restaurantId } = to.params;
+    this.fetchRestaurant(restaurantId);
+    next();
   },
   created() {
-      const { id:restaurantId } = this.$route.params
-      this.fetchRestaurant(restaurantId)
-  }
+    const { id: restaurantId } = this.$route.params;
+    this.fetchRestaurant(restaurantId);
+  },
 };
 </script>
