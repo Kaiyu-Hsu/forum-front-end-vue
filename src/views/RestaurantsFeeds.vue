59dab72d464f29e6db94590069c7bdea922420d6
Kaiyu <anne550323@gmail.com>
2021-11-02 17:04:29 +0800

    add: spinner

--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="container py-5">
     <NavTabs />
+    <Spinner v-if="isLoading" />
 
-    <h1 class="mt-5">最新動態</h1>
-    <hr />
-    <div class="row">
-      <div class="col-md-6">
-        <h3>最新餐廳</h3>
-        <!-- 最新餐廳 NewestRestaurants -->
-        <NewestRestaurants :restaurants="restaurants" />
+    <template v-else>
+      <h1 class="mt-5">最新動態</h1>
+      <hr />
+      <div class="row">
+        <div class="col-md-6">
+          <h3>最新餐廳</h3>
+          <!-- 最新餐廳 NewestRestaurants -->
+          <NewestRestaurants :restaurants="restaurants" />
+        </div>
+        <div class="col-md-6">
+          <!-- 最新評論 NewestComments-->
+          <h3>最新評論</h3>
+          <NewestComments :comments="comments" />
+        </div>
       </div>
-      <div class="col-md-6">
-        <!-- 最新評論 NewestComments-->
-        <h3>最新評論</h3>
-        <NewestComments :comments="comments" />
-      </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -23,6 +26,7 @@
 import NavTabs from "../component/NavTabs.vue";
 import NewestRestaurants from "../component/NewestRestaurants.vue";
 import NewestComments from "../component/NewestComments.vue";
+import Spinner from "./../component/Spinner.vue";
 import restaurantsAPI from "../apis/restaurants.js";
 import { Toast } from "./../utils/helpers";
 
@@ -32,11 +36,13 @@ export default {
     NavTabs,
     NewestRestaurants,
     NewestComments,
+    Spinner,
   },
   data() {
     return {
       restaurants: [],
       comments: [],
+      isLoading: true,
     };
   },
   created() {
@@ -55,7 +61,10 @@ export default {
         this.comments = comments.filter(
           (comment) => comment.Restaurant && comment.text
         );
+        // this.comments = comments;
+        this.isLoading = false;
       } catch (error) {
+        this.isLoading = false;
         console.log("error", error);
         Toast.fire({
           icon: "error",
