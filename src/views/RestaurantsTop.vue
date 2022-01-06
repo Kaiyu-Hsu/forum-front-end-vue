03cd1afe63c9aff603d7648e60e7613687737b7b
Kaiyu <anne550323@gmail.com>
2022-01-06 13:04:32 +0800

    add: 自製的spinner

--- src/views/RestaurantsTop.vue
+++ src/views/RestaurantsTop.vue
@@ -6,6 +6,7 @@
       人氣餐廳
     </h1>
     <hr />
+    <Spinner2 v-if="isLoading" />
     <!-- 人氣餐廳 -->
     <RestaurantsTopCards
       v-for="restaurant in restaurants"
@@ -18,6 +19,7 @@
 <script>
 import NavTabs from "../component/NavTabs.vue";
 import RestaurantsTopCards from "../component/RestaurantsTopCards.vue";
+import Spinner2 from "./../component/Spinner2.vue";
 import TopAPI from "./../apis/restaurants.js";
 import { Toast } from "./../utils/helpers.js";
 
@@ -26,10 +28,12 @@ export default {
   components: {
     NavTabs,
     RestaurantsTopCards,
+    Spinner2,
   },
   data() {
     return {
       restaurants: [],
+      isLoading: true,
     };
   },
   methods: {
@@ -39,7 +43,9 @@ export default {
         // console.log(response);
 
         this.restaurants = response.data.restaurants;
+        this.isLoading = false;
       } catch (error) {
+        this.isLoading = false;
         console.log(error);
         Toast.fire({
           icon: "warning",
